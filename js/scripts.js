70af74d1134fcd611d1d01b80de19c5f09e512b5
Marcos_Davi <104923141+lastergates@users.noreply.github.com>
2023-06-30 11:59:46 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,22 @@ class Validator{
 
     constructor(){
         this.validations = [
-            'data-min-legth',
+            'data-required',
+            'data-min-length',
+            'data-max-length',
+            
         ]
     }
 
     //iniciar a validação de todos os campos
     validate(form){
+
+        //resgata todas as validações
+        let currentValidations = document.querySelectorAll('form .erro-validation');
+
+        if(currentValidations.length){
+            this.cleanValidations(currentValidations);
+        }
 
         // pegar todos inputs
          let inputs = form.getElementsByTagName('input');
@@ -20,13 +30,82 @@ class Validator{
 
             //loop em todas as validações existentes
             for(let i=0; this.validations.length > i; i++){
-             if(input.getAttribute(this.validations[i]) !=null){
-                console.log('achou validação');
+                //verifica se a validação atual existe no input
+                if(input.getAttribute(this.validations[i]) !=null){
+                    
+                    // limpando a string para virar um método
+                    let method = this.validations[i].replace('data-','').replace('-','');
+
+                    //valor do input
+                    let value = input.getAttribute(this.validations[i]);
+
+                    //invocar o método
+                    this[method](input,value);
              }
             }
         }, this);
     }
 
+    //verifica se o input possui todos os caracteres
+    minlength(input, minValue){
+        let inputLength = input.value.length;
+
+        let errorMensage= `O campo precisa ter mais que ${minValue} caracteres`;
+
+        if(inputLength < minValue){
+            this.printMessage(input, errorMensage);
+        }
+
+    }
+
+    //verifica se o input passou do limite máximo de caracteres
+    maxlength(input, maxValue){
+        let inputLength = input.value.length;
+
+        let errorMensage= `O campo precisa ter menos que ${maxValue} caracteres`;
+
+        if(inputLength > maxValue){
+            this.printMessage(input, errorMensage);
+        }
+    }
+
+    //método que imprime mensagens de erros na tela
+    printMessage(input, msg){
+
+        //verifica a quantidade de erros
+        let errorsQty = input.parentNode.querySelector('.erro-validation');
+
+       if(errorsQty === null){
+            let template = document.querySelector('.erro-validation').cloneNode(true);
+
+            template.textContent = msg;
+
+            let inputParent = input.parentNode;
+
+            template.classList.remove('template');
+
+            inputParent.appendChild(template);
+       }
+
+    }
+
+    //verifica se o input é requerido
+    required(input){
+
+        let inputValue = input.value;
+
+        if(inputValue === ''){
+            let errorMensage = `Esse campo é obrigatório`;
+
+            this.printMessage(input, errorMensage);
+        }
+
+    }
+
+    //limpa as validações da tela
+    cleanValidations(validations){
+        validations.forEach(el => el.remove());
+    }
 }
 
 
